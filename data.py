ac0626b84230b6fc8e4cdd0a97e6eadfa76c8131
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-06-11 17:16:21 +0900

    お題1,2のひながたを作る

---
+++ data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from typing import *
+
+
+class product(NamedTuple):
+    id: int
+    name: str
+    price: int
+
+product_list = {
+    1: product(id=1, name="りんご", price=100),
+    2: product(id=2, name="みかん", price=40),
+    3: product(id=3, name="ぶどう", price=150),
+    4: product(id=4, name="のり弁", price=350),
+    5: product(id=5, name="しゃけ弁", price=400),
+    6: product(id=6, name="タバコ", price=420),
+    7: product(id=7, name="メンソールタバコ", price=440),
+    8: product(id=8, name="ライター", price=100),
+    9: product(id=9, name="お茶", price=80),
+}
+
+def product_list():
+    ringo = product_list[1]
+    print(ringo.id)    # 1
+    print(ringo.name)  # りんご
+    print(ringo.price) # 100
